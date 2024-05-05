4fb2b35cca63579043caf18d056193061adab89e
Chijindu Nwokeohuru <chiji4collage@gmail.com>
2024-05-05 18:56:36 +0100

    commit update 1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,21 +1,11 @@
 $(document).ready(function() {
-    var amenitiesChecked = {};
-
     $('input[type="checkbox"]').change(function() {
-        var amenityId = $(this).data('id');
-        var amenityName = $(this).data('name');
-
-        if (this.checked) {
-            amenitiesChecked[amenityId] = amenityName;
-        } else {
-            delete amenitiesChecked[amenityId];
-        }
-
-        updateAmenitiesList();
+        var selectedAmenities = [];
+        $('input[type="checkbox"]:checked').each(function() {
+            var amenityId = $(this).data('id');
+            var amenityName = $(this).data('name');
+            selectedAmenities.push(amenityName);
+        });
+        $('#amenities_text').text(selectedAmenities.join(', '));
     });
-
-    function updateAmenitiesList() {
-        var amenitiesList = Object.values(amenitiesChecked).join(', ');
-        $('div.Amenities h4').text(amenitiesList);
-    }
 });
